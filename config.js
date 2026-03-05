b4cc27eff1054aeced7887d5506cb08bdfe14df8
cinoxomultimedia <cinoxomultimediapvtltd@gmail.com>
2026-03-05 12:01:23 +0530

    integrated with backend fully

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
  * Global Configuration
  * Store variables like API endpoints here.
  */
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://maanoxo-engineering-backend-production-4719.up.railway.app';
 
